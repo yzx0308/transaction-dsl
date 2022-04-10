51f1e038102dbfa0094f08aad3c9471d676f9c10
yanxicheung <yanxicheung@foxmail.com>
2022-04-10 11:08:57 +0800

    remove defalut test

--- pt/run.sh
+++
@@ -1,2 +0,0 @@
-
-g++ -std=c++17 -O3 dsl-test.cpp nanobench.o -I/usr/local/include -I../../event/include -I../../cub/include -ltrans-dsl -levent
--- shared/defs.mk
+++
@@ -1,8 +0,0 @@
-
-TESTNGPP_ROOT=/usr/local
-MOCKCPP_ROOT=/usr/local
-PYTHON=/usr/bin/python
-
-testngpp-runner=$(TESTNGPP_ROOT)/bin/testngpp-runner
-testngpp-listener-path=$(TESTNGPP_ROOT)/testngpp/listener
-
