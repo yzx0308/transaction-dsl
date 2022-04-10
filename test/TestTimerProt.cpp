1cd16788c61ab676f54e584ad736b2454052ac10
yanxicheung <yanxicheung@foxmail.com>
2022-04-10 20:55:12 +0800

    add transaction & time prot test

---
+++ test/TestTimerProt.cpp
@@ -0,0 +1,117 @@
+#include "gtest/gtest.h"
+
+#include "trans-dsl/TransactionDSL.h"
+#include "event/impl/SimpleEventInfo.h"
+#include "event/concept/Event.h"
+#include "trans-dsl/action/SimpleAsyncActionHelper.h"
+#include <event/impl/ConsecutiveEventInfo.h>
+#include <trans-dsl/TslStatus.h>
+#include "trans-dsl/action/Actor.h"
+
+using namespace cub;
+using namespace tsl;
+using namespace ev;
+
+struct MyTimerInfo : TimerInfo
+{
+   U32 getTimerLen(const TimerId) const
+   {
+      return 10;
+   }
+};
+
+enum: Status
+{
+    ERROR1 = failStatus(200),
+    ERROR2,
+    ERROR3,
+    ERROR4
+};
+
+#define EVENT(n) SimpleEventInfo(n)
+
+#define TIMER_EVENT(n) EVENT(n + 400)
+
+struct TimerProt1: testing::Test
+{
+protected:
+    MyTimerInfo timerInfo;
+
+    __transaction
+    ( __timer_prot(1, __wait(1))
+    )trans;
+private:
+    void SetUp() override
+    {
+        trans.updateTimerInfo(timerInfo);
+        ASSERT_EQ(TSL_CONTINUE, trans.start());
+    }
+
+    void TearDown() override
+    {
+
+    }
+};
+
+// @test(id="event-1")
+TEST_F(TimerProt1, after_recv_event1_should_return_TSL_SUCCESS)
+{
+    ASSERT_EQ(TSL_SUCCESS, trans.handleEvent(EVENT(1)));
+}
+
+// @test(id="timeout")
+TEST_F(TimerProt1, after_recv_timer1_should_return_TIMEDOUT)
+{
+    ASSERT_EQ(TSL_TIMEDOUT, trans.handleEvent(TIMER_EVENT(1)));
+}
+
+// @test(depends="timeout")
+TEST_F(TimerProt1, after_timeout_if_recv_event1_should_return_UNKNOWN_EVENT)
+{
+    ASSERT_EQ(TSL_TIMEDOUT, trans.handleEvent(TIMER_EVENT(1)));
+    ASSERT_EQ(TSL_UNKNOWN_EVENT, trans.handleEvent(EVENT(1)));
+}
+
+// @test(depends="event-1")
+TEST_F(TimerProt1, after_recv_event1_if_timeout_should_return_UNKNOWN_EVENT)
+{
+    ASSERT_EQ(TSL_SUCCESS, trans.handleEvent(EVENT(1)));
+    ASSERT_EQ(TSL_UNKNOWN_EVENT, trans.handleEvent(TIMER_EVENT(1)));
+}
+
+TEST_F(TimerProt1, if_stop_should_return_stop_cause)
+{
+    ASSERT_EQ(ERROR2, trans.stop(ERROR2));
+}
+
+
+struct TimerProt2: testing::Test
+{
+protected:
+    MyTimerInfo timerInfo;
+
+    __transaction
+    ( __timer_prot(1, __wait(1), ERROR1)
+    )trans;
+private:
+    void SetUp() override
+    {
+        trans.updateTimerInfo(timerInfo);
+        ASSERT_EQ(TSL_CONTINUE, trans.start());
+    }
+
+    void TearDown() override
+    {
+
+    }
+};
+
+TEST_F(TimerProt2, after_recv_timer1_should_return_ERROR1)
+{
+    ASSERT_EQ(ERROR1, trans.handleEvent(TIMER_EVENT(1)));
+}
+
+TEST_F(TimerProt2, if_stop_should_return_stop_cause)
+{
+    ASSERT_EQ(ERROR2, trans.stop(ERROR2));
+}
--- test/TestTransaction.cpp
+++ test/TestTransaction.cpp
@@ -20,12 +20,13 @@ using namespace ev;
 
 namespace cub
 {
-  extern void log_error(const char* file, unsigned int line, const char* fmt, ...)
-  {
-  }
+    extern void log_error(const char* file,
+                            unsigned int line,
+                            const char* fmt,
+                            ...){}
 }
 
-enum : Status
+enum: Status
 {
     ERROR1 = failStatus(200),
     ERROR2,
@@ -33,28 +34,110 @@ enum : Status
     ERROR4
 };
 
-
-struct MyContext : private SimpleRuntimeContext, SimpleTransactionContext
+struct MyContext: private SimpleRuntimeContext, SimpleTransactionContext
 {
-   MyContext() : SimpleTransactionContext((RuntimeContext&)*this)
-   {
-   }
+    MyContext()
+    : SimpleTransactionContext((RuntimeContext&) *this)
+    {
+    }
 };
 
 #define EVENT(n) SimpleEventInfo(n)
 
-struct Sequential:testing::Test
+// @fixture(tags="seq")
+struct Sequential: testing::Test
+{
+protected:
+    __transaction
+    ( __sequential
+    ( __wait(1)
+    , __wait(2))
+    )trans;
+private:
+    void SetUp() override
+    {
+        ASSERT_EQ(TSL_CONTINUE, trans.start());
+    }
+
+    void TearDown() override
+    {
+
+    }
+};
+
+/*SUCCESS:trans 成功结束
+ *CONTINUE:trans 仍在工作
+ *UNKNOWN_EVENT:trans收到一个未期待的消息
+ *错误码:trans失败并且终止
+ * */
+TEST_F(Sequential,after_recv_event1_should_return_TSL_CONTINUE)
+{
+    ASSERT_EQ(TSL_CONTINUE, trans.handleEvent(EVENT(1)));
+}
+
+TEST_F(Sequential,after_recv_event2_should_return_UNKNOWN_EVENT)
+{
+    ASSERT_EQ(TSL_UNKNOWN_EVENT, trans.handleEvent(EVENT(2)));
+}
+
+TEST_F(Sequential,after_recv_event1_if_recv_event2_should_return_TSL_SUCCESS)
+{
+    ASSERT_EQ(TSL_CONTINUE, trans.handleEvent(EVENT(1)));
+    ASSERT_EQ(TSL_SUCCESS, trans.handleEvent(EVENT(2)));
+}
+
+TEST_F(Sequential,after_stop_should_return_TSL_SUCCESS)
+{
+    ASSERT_EQ(TSL_FORCE_STOPPED, trans.stop());
+}
+
+TEST_F(Sequential,after_stop_if_recv_event1_should_return_UNKNOWN_EVENT)
+{
+    ASSERT_EQ(TSL_FORCE_STOPPED, trans.stop());
+    ASSERT_EQ(TSL_UNKNOWN_EVENT, trans.handleEvent(EVENT(1)));
+}
+
+TEST_F(Sequential,after_kill_if_recv_event1_should_return_UNKNOWN_EVENT)
+{
+    trans.kill();
+    ASSERT_EQ(TSL_UNKNOWN_EVENT, trans.handleEvent(EVENT(1)));
+}
+
+// @fixture(tags="seq throw")
+struct Sequential2: testing::Test
 {
 protected:
     __transaction
     ( __sequential
         ( __wait(1)
+        , __throw(ERROR1)
         , __wait(2))
     )trans;
+private:
+    void SetUp() override
+    {
+        ASSERT_EQ(TSL_CONTINUE, trans.start());
+    }
+
+    void TearDown() override
+    {
+
+    }
 };
 
-TEST_F(Sequential,should_equal)
+TEST_F(Sequential2, after_recv_event1_should_return_ERROR1)
 {
+    ASSERT_EQ(ERROR1, trans.handleEvent(EVENT(1)));
+}
 
+TEST_F(Sequential2, after_recv_event2_should_return_UNKNOWN_EVENT)
+{
+    ASSERT_EQ(TSL_UNKNOWN_EVENT, trans.handleEvent(EVENT(2)));
+}
+
+TEST_F(Sequential2, after_recv_event1_if_recv_event2_should_return_UNKNOWN_EVENT)
+{
+    ASSERT_EQ(ERROR1, trans.handleEvent(EVENT(1)));
+    ASSERT_EQ(TSL_UNKNOWN_EVENT, trans.handleEvent(EVENT(2)));
 }
 
